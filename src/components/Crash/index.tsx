1dec3e243df47c00f2712791bcf486d657222a8b
Emma <emma@coinhe.io>
2022-10-04 16:51:52 +0700

    update line color + smooth line

--- src/components/Crash/index.tsx
+++ src/components/Crash/index.tsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { Chart } from "react-chartjs-2";
+import { Line, Chart } from "react-chartjs-2";
 import "chart.js/auto";
 import "chartjs-adapter-moment";
-import Axios from "axios";
 import { connect } from "socket.io-client";
 import "./Crash.scss";
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 const API_BASE = "http://localhost:4000";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 const Crash = () => {
   const [chartData, setChartData] = useState<any>({ datasets: [] });
   const [chartOptions, setChartOptions] = useState({});
@@ -21,32 +38,42 @@ const Crash = () => {
   const [roundIdList, setRoundIdList] = useState([]);
   const [bBettingPhase, setbBettingPhase] = useState(false);
   const [bettingPhaseTime, setBettingPhaseTime] = useState<any>(-1);
-
-  //   const [liveBettingTable, setLiveBettingTable] = useState<any>();
   const [hookToNextRoundBet, setHookToNextRoundBet] = useState(false);
-  //   const [tenNumbers, setTenNumbers] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   const multiplierCount = useRef<any>([]);
   const timeCount_xaxis = useRef<any>([]);
   const realCounter_yaxis = useRef<any>(5);
-  useEffect(() => {
-    let bettingInterval: any = null;
-
-    if (bBettingPhase) {
-      bettingInterval = setInterval(() => {
-        let time_elapsed = (Date.now() - globalTimeNow) / 1000.0;
-        let time_remaining: any = (5 - time_elapsed).toFixed(2);
-        setBettingPhaseTime(time_remaining);
-        if (time_remaining < 0) {
-          setbBettingPhase(false);
-        }
-      }, 10);
+  const chartRef = useRef();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const draw = (chartRef: any, position: number) => {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas?.getContext("2d");
+    if (ctx) {
+      ctx.beginPath();
+      ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+      ctx.stroke();
     }
-    return () => {
-      clearInterval(bettingInterval);
-      setBettingPhaseTime("Starting...");
-    };
-  }, [bBettingPhase]);
+  };
+
+  // useEffect(() => {
+  //   let bettingInterval: any = null;
+
+  //   if (bBettingPhase) {
+  //     bettingInterval = setInterval(() => {
+  //       let time_elapsed = (Date.now() - globalTimeNow) / 1000.0;
+  //       let time_remaining: any = (5 - time_elapsed).toFixed(2);
+  //       setBettingPhaseTime(time_remaining);
+  //       if (time_remaining < 0) {
+  //         setbBettingPhase(false);
+  //       }
+  //     }, 1);
+  //   }
+  //   return () => {
+  //     clearInterval(bettingInterval);
+  //     setBettingPhaseTime("Starting...");
+  //   };
+  // }, [bBettingPhase]);
+
   useEffect(() => {
     const temp_interval = setInterval(() => {
       setChartSwitch(false);
@@ -58,6 +85,20 @@ const Crash = () => {
       setChartSwitch(true);
     };
   }, [chartSwitch]);
+
+  function getGradient(ctx: any, chartArea: any) {
+    let gradient = ctx.createLinearGradient(
+      0,
+      chartArea.bottom,
+      0,
+      chartArea.top
+    );
+    gradient.addColorStop(0, "#F4E38B");
+    gradient.addColorStop(0.5, "#C49939");
+    gradient.addColorStop(1, "#B27F2A");
+
+    return gradient;
+  }
   // Chart Data
   const sendToChart = () => {
     setChartData({
@@ -67,69 +108,80 @@ const Crash = () => {
         {
           data: multiplierCount.current,
           backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgba(75,192,192,1)",
+          borderColor: function (context: any) {
+            const chart = context.chart;
+            const { ctx, chartArea } = chart;
+            if (!chartArea) {
+              // This case happens on initial chart load
+              return;
+            }
+            return getGradient(ctx, chartArea);
+          },
           color: "rgba(0,0,0,1)",
 
           pointRadius: 0,
-          borderDash: [35, 0],
-          lineTension: 0.1,
+          borderDash: [],
+          lineTension: 0.4,
         },
       ],
     });
     setChartOptions({
       events: [],
-      maintainAspectRatio: false,
-      elements: {
-        line: {
-          tension: 0.1,
-        },
-      },
+      // maintainAspectRatio: false,
+      // elements: {
+      //   line: {
+      //     tension: 0.4,
+      //   },
+      // },
+      responsive: true,
       scales: {
         yAxes: {
           type: "linear",
-
-          title: {
-            display: false,
-            text: "value",
-          },
           min: 0,
           max: liveMultiplier > 2 ? liveMultiplier : 2,
+          afterTickToLabelConversion: function (scaleInstance: any) {
+            scaleInstance.ticks[0].label = "";
+
+            scaleInstance.ticks[scaleInstance.ticks.length - 1].label = "";
+          },
           ticks: {
             color: "rgba(255, 255, 255,1)",
-            maxTicksLimit: 5,
+            maxTicksLimit: 6,
             callback: function (value: any, index: any, values: any) {
-              if (value % 0.5 == 0) return parseFloat(value).toFixed(2);
+              // if (value >= 2 && value % 2 == 0)
+              return +parseFloat(value).toFixed(1) + "x";
+              // if (value < 2 && value >= 1.2) {
+              //   return +parseFloat(value).toFixed(1) + "x";
+              // }
             },
           },
           grid: {
             display: false,
-            // color: "white",
           },
         },
         xAxes: {
           type: "linear",
-          title: {
-            display: false,
-            text: "value",
-          },
+          min: 0,
           max: gamePhaseTimeElapsed > 8 ? gamePhaseTimeElapsed : 8,
           ticks: {
             color: "rgba(255, 255, 255,1)",
-
-            maxTicksLimit: 5,
+            maxTicksLimit: 10,
             callback: function (value: any, index: any, values: any) {
               if (gamePhaseTimeElapsed < 10) {
                 if (value % 1 == 0) return value;
               } else {
-                if (value % 10 == 0) return value;
+                if (value % 5 == 0) return value;
               }
             },
           },
           grid: {
             display: false,
-            // color: "orange",
           },
         },
+      },
+      interaction: {
+        mode: "index",
+        intersect: false,
       },
       plugins: {
         legend: { display: false },
@@ -153,10 +205,6 @@ const Crash = () => {
       },
     });
   };
-
-  //   const canvasRef = useRef<HTMLCanvasElement>(null);
-  //   const canvasWidth = 708;
-  //   const canvasHeight = 282;
   useEffect(() => {
     let gameCounter: any = null;
     if (liveMultiplierSwitch) {
@@ -168,16 +216,18 @@ const Crash = () => {
         setLiveMultiplier((1.0024 * Math.pow(1.0718, time_elapsed)).toFixed(2));
 
         if (multiplierCount.current.length < 1) {
-          multiplierCount.current = multiplierCount.current.concat([1]);
+          multiplierCount.current = multiplierCount.current.concat([0]);
           timeCount_xaxis.current = timeCount_xaxis.current.concat([0]);
         }
         if (realCounter_yaxis.current % 5 == 0) {
           multiplierCount.current = multiplierCount.current.concat([
-            (1.0024 * Math.pow(1.0718, time_elapsed)).toFixed(2),
+            1.0024 * Math.pow(1.0718, time_elapsed) - 1,
           ]);
           timeCount_xaxis.current = timeCount_xaxis.current.concat([
             time_elapsed,
           ]);
+          // draw(chartRef, multiplierCount.current.length);
+          // console.log(chartRef);
         }
         realCounter_yaxis.current += 1;
       }, 1);
@@ -188,13 +238,8 @@ const Crash = () => {
   }, [liveMultiplierSwitch]);
   // Socket.io setup
   useEffect(() => {
-    // retrieve();
     const socket = connect("http://localhost:3001");
     setGlobalSocket(socket);
-
-    // socket.on("news_by_server", function (data) {
-    //   setAnnouncement(data);
-    // });
 
     socket.on("start_multiplier_count", function (data) {
       setGlobalTimeNow(Date.now());
@@ -206,33 +251,9 @@ const Crash = () => {
       setLiveMultiplierSwitch(false);
 
       setBetActive(false);
+      multiplierCount.current = [];
+      timeCount_xaxis.current = [];
     });
-
-    // socket.on("update_user", function (data) {
-    //   getUser();
-    // });
-
-    // socket.on("crash_history", function (data) {
-    //   setCrashHistory(data);
-
-    //   let temp_streak_list = [];
-    //   const new_data = data;
-    //   let blue_counter = 0;
-    //   let red_counter = 0;
-
-    //   for (let i = 0; i < data.length; i++) {
-    //     if (new_data[i] >= 2) {
-    //       blue_counter += 1;
-    //       red_counter = 0;
-    //       temp_streak_list.push(blue_counter);
-    //     } else {
-    //       red_counter += 1;
-    //       blue_counter = 0;
-    //       temp_streak_list.push(red_counter);
-    //     }
-    //   }
-    //   setStreakList(temp_streak_list.reverse());
-    // });
 
     socket.on("get_round_id_list", function (data) {
       setRoundIdList(data.reverse());
@@ -242,7 +263,7 @@ const Crash = () => {
       setGlobalTimeNow(Date.now());
       setLiveMultiplier("Starting...");
       setbBettingPhase(true);
-      //   setLiveBettingTable(null);
+      // setLiveBettingTable(null);
       setHookToNextRoundBet(true);
       //   retrieve_active_bettors_list();
 
@@ -250,78 +271,42 @@ const Crash = () => {
       timeCount_xaxis.current = [];
     });
 
-    // socket.on("receive_message_for_chat_box", (data) => {
-    //   get_chat_history();
-    // });
-
-    // socket.on("receive_live_betting_table", (data) => {
-    //   setLiveBettingTable(data);
-    //   data = JSON.parse(data);
-    //   setTenNumbers(Array(10 - data.length).fill(2));
-    // });
-
     return () => {
       socket.disconnect();
     };
   }, []);
   useEffect(() => {
-    // get_game_status();
-    // getUser();
     setChartSwitch(true);
-    // let getChatHistoryTimer = setTimeout(() => get_chat_history(), 1000);
-    // let getActiveBettorsTimer = setTimeout(
-    //   () => retrieve_active_bettors_list(),
-    //   1000
-    // );
-    // let getBetHistory = setTimeout(() => retrieve_bet_history(), 1000);
+  }, []);
 
-    // return () => {
-    //   clearTimeout(getChatHistoryTimer);
-    //   clearTimeout(getActiveBettorsTimer);
-    //   clearTimeout(getBetHistory);
-    // };
-  }, []);
-  useEffect(() => {
-    // setChartSwitch(true);
-    // let x = 0;
-    // let y = canvasHeight;
-    // const canvas = canvasRef.current;
-    // if (canvas) {
-    //   const ctx = canvas.getContext("2d");
-    //   if (ctx) {
-    //     ctx.beginPath();
-    //     ctx.moveTo(0, canvasHeight);
-    //     // ctx.lineTo(canvasWidth, 0);
-    //     ctx.lineWidth = 3;
-    //     let grd = ctx.createLinearGradient(90, 90, 120, 0);
-    //     grd.addColorStop(0, "#FFF");
-    //     grd.addColorStop(1, "#BF55EC");
-    //     ctx.strokeStyle = grd;
-    //     for (let i = 0; i < canvasWidth; i++) {
-    //       ctx.moveTo(0, canvasHeight);
-    //       x = i;
-    //       //   y = Math.pow(1.1, x);
-    //       y -= 0.25;
-    //       ctx.lineTo(x, y);
-    //     }
-    //     ctx.stroke();
-    //     ctx.scale(0.5, 1);
-    //   }
-    // }
-  }, []);
   return (
     <div className="effects-box">
-      <div
-        className="basically-the-graph"
-        //   style={{
-        //     height: "90%",
-        //     width: "90%",
-        //     position: "absolute",
-        //     top: "12%",
-        //   }}
-      >
+      <div className="basically-the-graph">
         {chartData ? (
-          <Chart type="line" data={chartData} options={chartOptions} />
+          <>
+            <Chart
+              type="line"
+              data={chartData}
+              options={chartOptions}
+              ref={chartRef}
+            />
+            {/* <motion.canvas
+              ref={canvasRef}
+              width="500"
+              height="500"
+              style={{
+                position: "absolute",
+                zIndex: 10,
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                background: "transparent",
+              }}
+              // animate={{ rotate: rotate }}
+              // transition={{ ease: "easeOut", duration: easeOut }}
+            /> */}
+          </>
         ) : (
           ""
         )}
